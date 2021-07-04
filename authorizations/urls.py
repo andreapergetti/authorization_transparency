c95c578e2ca156e4eeca220a62c6fcc6c0f51cb7
andrea <andrea@debian.com>
2021-07-04 19:11:15 +0200

    Add the Authorization delete functionality

--- authorizations/urls.py
+++ authorizations/urls.py
@@ -6,5 +6,6 @@ app_name = 'authorizations'
 urlpatterns = [
     path('authorization/create', views.AuthorizationCreate.as_view(), name='authorization_create'),
     path('authorization/<int:pk>/detail', views.AuthorizationDetail.as_view(), name='authorization_detail'),
+    path('authorization/<int:pk>/delete', views.AuthorizationDelete.as_view(), name='authorization_delete'),
     path('authorization/list', views.AuthorizationList.as_view(), name='authorization_list'),
 ]
--- authorizations/views.py
+++ authorizations/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import FormView, CreateView, ListView
+from django.views.generic import FormView, CreateView, ListView, DeleteView
 from django.views.generic.detail import DetailView
 from authorizations.models import Authorizations
 from .forms import AuthorizationForm
@@ -19,6 +19,12 @@ class AuthorizationCreate(CreateView):
     template_name = 'authorizations/authorization_create.html'
     success_url = reverse_lazy('homepage')
     form_class = AuthorizationForm
+
+
+class AuthorizationDelete(DeleteView):
+    model = Authorizations
+    template_name = 'authorizations/authorization_delete.html'
+    success_url = reverse_lazy('authorizations:authorization_list')
 
 
 class AuthorizationList(ListView):
